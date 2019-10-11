8d509011e030b42b4de7cbadc8e053379028e7fe
carolkes <carolkestering@hotmail.com>
2019-10-11 10:27:42 -0300

    commit com comentários na controller

--- Solution1/InfomacoesFlores/Controller/FloresController.cs
+++ Solution1/InfomacoesFlores/Controller/FloresController.cs
@@ -12,24 +12,36 @@ namespace InfomacoesFlores.Controller
         //adicionando estância nova para context
         FloresContext contextDB = new FloresContext();
 
-        //Listando as flores
+        /// <summary>
+        /// Listagem das flores
+        /// </summary>
+        /// <returns>Retorna Lista de todas as flores</returns>
         public IQueryable<Flor> GetFlores()
         {
             return contextDB.Flores;
         }
         /// <summary>
-        /// inserindo as flores
+        /// Inserindo as flores
         /// </summary>
-        /// <param name="item"></param>
+        /// <param name="item">Insere novas flores no sistema</param>
         public void InserirFlor(Flor item)
         {
             contextDB.Flores.Add(item);
             contextDB.SaveChanges();
         }
+        /// <summary>
+        /// Ordena as flores
+        /// </summary>
+        /// <returns>Retorna lista ordenada da quantidade de flores da maior 
+        /// para a menor</returns>
         public IQueryable<Flor> OrdenarFlor()
         {
             return contextDB.Flores.OrderByDescending(x => x.Quantidade);
         }
+        /// <summary>
+        /// Soma a quantidade total de flores
+        /// </summary>
+        /// <returns>retorna a soma totam da quantidade de flores</returns>
         public int SomaFlor()
         {
             return contextDB.Flores.Sum(x => x.Quantidade);
